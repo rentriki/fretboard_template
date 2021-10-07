012bbd4c0798ebd3ebbd7a11f5bccc40012617d6
Russell Entrikin <russell.entrikin@gmail.com>
2021-10-06 21:33:12 -0400

    better configurability

--- draw_frets.py
+++ draw_frets.py
@@ -1,69 +1,92 @@
+import json
+import os
+
+from dataclasses import dataclass
+
 import cairo 
 
-DPI = 108
-EDO = 22
-FRETBOARD_LENGTH = 25
-SCALE_LENGTH = 34
+DPI = 72
+BUFFER = int(DPI/4)
+WIDTH = (8.5 * DPI) - (2 * BUFFER)
+HEIGHT = (11 * DPI) - (2 * BUFFER)
+MIN_X = BUFFER
+MAX_X = WIDTH - BUFFER
+MIN_Y = BUFFER
+MAX_Y = HEIGHT - BUFFER
+CENTER_X = (MAX_X - MIN_X) / 2
+
+@dataclass
+class FretboardConfig:
+    edo: float
+    fretboard_length: float
+    scale_length: float
+
+def read_config(infilename):
+    with open(infilename) as infile:
+        return FretboardConfig(**json.loads(infile.read()))
 
 def in2cm(inches): 
     return 2.54 * inches
 
-def fret_spacing(fret_num, scale_len=SCALE_LENGTH, tet_num=EDO):
-    return scale_len * (1 - 2**(-fret_num/tet_num))
+def fret_spacing(fret_num, config):
+    return config.scale_length * (1 - 2**(-fret_num/config.edo))
 
-def all_frets(fretboard_len=FRETBOARD_LENGTH, **kwargs):
+def all_frets(config):
     i = 0
-    fret = fret_spacing(i, **kwargs)
-    while fret < fretboard_len:
+    fret = fret_spacing(i, config)
+    while fret < config.fretboard_length:
         yield fret
         i += 1
-        fret = fret_spacing(i, **kwargs)
+        fret = fret_spacing(i, config)
 
-def write_frets(context, frets, line_len=DPI):
+def write_frets(context, frets, line_length):
     for i, fret in enumerate(frets):
-        context.move_to(700, int(fret*DPI))
-        context.line_to(700-line_len, int(fret*DPI))
-        context.move_to(700-(line_len+20), int(fret*DPI)+20)
+        context.move_to(CENTER_X-(line_length/2), MIN_Y+fret*DPI)
+        context.line_to(CENTER_X+(line_length/2), MIN_Y+fret*DPI)
+        context.move_to(CENTER_X-(line_length/2)-(DPI/4), 
+                        MIN_Y+(fret*DPI)+(DPI/4))
         context.show_text(str(i))
 
-def print_frets(fretboard_len=FRETBOARD_LENGTH, 
-                scale_len=SCALE_LENGTH, tet_num=EDO,
-                outfilename=f"{EDO}edo.svg", 
-                width=8.5*DPI, height=FRETBOARD_LENGTH*DPI):
+def print_frets(config, outfilename):
     
-    with cairo.SVGSurface(outfilename, width, height) as surface:
+    with cairo.SVGSurface(outfilename, WIDTH, HEIGHT) as surface:
         
         context = cairo.Context(surface)
-        context.set_font_size(30)
-
-        context.set_line_width(1)
-        context.set_source_rgb(50, 0, 0)
-        twelve_edo_frets = all_frets(fretboard_len=fretboard_len,
-                                     scale_len=scale_len,
-                                     tet_num=12)
-        write_frets(context, twelve_edo_frets, line_len=DPI*2)
-        context.stroke()
 
         context.set_line_width(1)
         context.set_source_rgb(0, 255, 0)
-        context.move_to(0, 20)
-        context.line_to(100, 20)
-        context.move_to(0, 20+int(DPI))
-        context.line_to(100, 20+int(DPI))
-        context.move_to(140, 40+int(DPI))
+        context.move_to(MIN_X, MIN_Y)
+        context.line_to(MIN_X+(DPI/4), MIN_Y)
+        context.move_to(MIN_X, MIN_Y+DPI)
+        context.line_to(MIN_X+(DPI/4), MIN_Y+DPI)
+        context.move_to(MIN_X, DPI)
+        context.set_font_size(10)
         context.show_text(f'1 inch = {DPI}px')
+        context.stroke()
+
+        context.set_line_width(1)
+        context.set_source_rgb(50, 0, 0)
+        context.set_font_size(26)
+        twelve_edo_config = FretboardConfig(12, config.fretboard_length,
+                                                config.scale_length)
+        write_frets(context, all_frets(twelve_edo_config), DPI*2)
         context.stroke()
         
         context.set_line_width(2)
         context.set_source_rgb(0, 0, 0)
-        target_frets = all_frets(fretboard_len=fretboard_len,
-                                 scale_len=scale_len,
-                                 tet_num=tet_num)
-        write_frets(context, target_frets, line_len=DPI*3)
+        target_frets = all_frets(config)
+        write_frets(context, target_frets, DPI*3)
         context.stroke()
     
     print(f"Image saved as {outfilename}.")
 
 if __name__ == '__main__':
 
-    print_frets()
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Draw a fretboard.')
+    parser.add_argument('config_filename')
+    args = parser.parse_args()
+
+    outfilename = os.path.basename(args.config_filename).split('.')[0] + '.svg'
+    print_frets(read_config(args.config_filename), outfilename)
